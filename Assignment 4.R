3067da3a4983e4f671349a5587996b4e899decbd
Ramiz <ramizk2001@gmail.com>
2023-07-08 15:29:25 -0400

    cleaned durations.seconds

--- Assignment 4.R
+++ Assignment 4.R
@@ -44,18 +44,43 @@ kable(hoax_sightings_per_country,
       caption = "Values are in percentile, %, format\n
       --LEGEND--\n au = Australia\n ca = Canada\n de = Denmark\n gb = Great Britain\n us = United States of America")
 
-df$report_delay <- as.numeric(df$date_posted - df$datetime) / (60*60*24)  
+df$report_delay_days <- ceiling(as.numeric(df$date_posted - df$datetime) / (60*60*24))  
 
 
 # While numeric, removing any observations with a negative report_delay value, meaning it was reported before it happened
 
-df <- df[df$report_delay > 0, ]
-
-df$report_delay <- paste0(floor(df$report_delay), " days")  
+df <- df[df$report_delay_days >= 0, ]
     
 report_delay_per_country <- df %>%
+  filter(!is.na(report_delay_days)) %>%
   group_by(country) %>%
+  summarise(average_report_delay = mean(report_delay_days))
+
+### Issues with durations.seconds column ###
+# duration.hours.min is a character class while duration.seconds is a numeric class
+# class conversion can be annoying when dealing with operations on values
+# when durations.hours.min is estimated (ex. 10-15 mins), duration.seconds is listed to the upper limit of the estimate (ex. 15 mins = 900 seconds)
+  # This is a weird one obs(25650) has listed 45 seconds - 2 hours, and duration.seconds decided to make it 7200 (2 hours in seconds) + 45 seconds to be 7245 seconds
+# when durations.hours.mins uses operators such as ~ > <, durations.seconds just takes the numeric value attached to the operator
+# "seconds" is automatically to be interpreted as 2.00 seconds
+# words such as several, split, multiple, no more than, do not work well when converted into seconds
+# approx and about keywords are fine and work well in converting to seconds
+
+# Dealing with durations.seconds
+
+# removing durations.seconds values for durations.hours.min values that have words, "several", "split", "multiple", "no more than", "or more", "still going"
+# removing duration.seconds values for durations.hours.min values that are ranges (ex. 10-15 mins) by filtering for "-" or "to"
+# removing duration.seconds values for durations.hours.min values that are "days", "evening", "week", "morning", "month"
+# single observation has a typo, 23000 instead of 2300, removed that string too just to avoid heavy outliers
+
+
+# Set values as NA based on the presence of multiple strings in another column
+exclusion_words <- c("several", "split", "multiple", "no more than", "or more", "still going", "-", "to", "days", "evening", "week", "morning", "month", "23000")
+
+df <- df %>%
+  mutate(duration.seconds = ifelse(grepl(paste(exclusion_words, collapse = "|"), tolower(duration.hours.min)), NA, duration.seconds))
+
       
-View(average_report_delay_per_country)
+View(report_delay_per_country)
 View(df)  
 
